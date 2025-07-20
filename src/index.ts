ae24027bfdca7cb1bd810a5b7a5cee20551e84d8
goemktg <samktg52@gmail.com>
2025-07-20 07:16:10 +0000

    refactor: pre- reimplementation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Client, Events } from "discord.js";
+import { Client } from "discord.js";
 import {
   loadEnvironmentVariables,
   setDefaultLogLevel,
 } from "./library/functions";
-import { CommandsHandler } from "./library/classes/CommandHandler";
-import { PriceHandler } from "./classes/PriceHandler";
+import { CommandsHandler } from "./library/classes/CommandsHandler";
+import { DatabaseEngine } from "./library/classes/DatabaseEngine";
 
 loadEnvironmentVariables();
 setDefaultLogLevel();
@@ -12,37 +12,12 @@ setDefaultLogLevel();
 const client = new Client({
   intents: [],
 });
-const commandsHandler = new CommandsHandler();
+client.databaseEngine = new DatabaseEngine();
 
 void client.login(process.env.DISCORD_TOKEN);
 
 client.once("ready", () => {
   void (async () => {
-    client.commands = await commandsHandler.getCommandsFromDir();
-  })();
-});
-
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  commandsHandler.executeCommand(interaction).catch(console.error);
-});
-
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isButton() || !interaction.customId.startsWith("price:"))
-    return;
-
-  // console.log(
-  //   interaction.customId.split(":")[1],
-  //   interaction.customId.split(":")[2],
-  // );
-  void (async () => {
-    const priceHandler = new PriceHandler();
-    await interaction.deferReply();
-    await priceHandler.replyPrice(
-      interaction,
-      interaction.customId.split(":")[2],
-      interaction.customId.split(":")[1],
-    );
+    client.commands = await CommandsHandler.getCommandsFromDir();
   })();
 });
